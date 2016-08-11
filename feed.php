8cc7da48c2eec4e59d90eb9f134009fd4f86c6b5
Кладовая <r57zone@gmail.com>
2016-08-11 22:30:11 +0400

    Fix tab

--- feed.php
+++ feed.php
@@ -7,30 +7,34 @@ $headers=stream_context_create($headers_a);
 if ($url!='http://www.lostfilm.tv/rssdd.xml') $url=str_replace('!',"&amp;",$url);
 $source=file_get_contents($url, false, $headers);
 //Обновление ленты
-if ($url=='http://www.lostfilm.tv/rssdd.xml') {
-$source=str_replace('windows-1251','utf-8',$source); 
-$source=iconv("WINDOWS-1251", "UTF-8", $source);
-$source=str_replace('<link>https://www.lostfilm.tv/download.php',"<link>http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]?url=http://lostfilm.tv/download.php",$source);
-$source=str_replace('&amp;',"!",$source);}
-if (strpos(strtolower($url), '.torrent')>0) {
-//Получаем имя торрента
-$filename=substr($url,strpos($url,'&amp;')+5,strlen($url));
-//Сохраняем торрент
-file_put_contents($filename, $source);
-if (ob_get_level()) ob_end_clean();
-//Заголовки для отдаваемого торрента
-header('Content-Description: File Transfer');
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename=' . basename($filename));
-header('Content-Transfer-Encoding: binary');
-header('Expires: 0');
-header('Cache-Control: must-revalidate');
-header('Pragma: public');
-header('Content-Length: ' . filesize($filename));
-//Отдаем файл
-readfile($filename);
-//Удаляем торрент
-unlink($_SERVER['DOCUMENT_ROOT'].'/'.$filename);
-exit;} 
+if ($url=='http://www.lostfilm.tv/rssdd.xml')
+  {
+  $source=str_replace('windows-1251','utf-8',$source); 
+  $source=iconv("WINDOWS-1251", "UTF-8", $source);
+  $source=str_replace('<link>https://www.lostfilm.tv/download.php',"<link>http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]?url=http://lostfilm.tv/download.php",$source);
+  $source=str_replace('&amp;',"!",$source);
+  }
+if (strpos(strtolower($url), '.torrent')>0)
+  {
+  //Получаем имя торрента
+  $filename=substr($url,strpos($url,'&amp;')+5,strlen($url));
+  //Сохраняем торрент
+  file_put_contents($filename, $source);
+  if (ob_get_level()) ob_end_clean();
+  //Заголовки для отдаваемого торрента
+  header('Content-Description: File Transfer');
+  header('Content-Type: application/octet-stream');
+  header('Content-Disposition: attachment; filename=' . basename($filename));
+  header('Content-Transfer-Encoding: binary');
+  header('Expires: 0');
+  header('Cache-Control: must-revalidate');
+  header('Pragma: public');
+  header('Content-Length: ' . filesize($filename));
+  //Отдаем файл
+  readfile($filename);
+  //Удаляем торрент
+  unlink($_SERVER['DOCUMENT_ROOT'].'/'.$filename);
+  exit;
+  } 
 else echo $source;
 ?>
