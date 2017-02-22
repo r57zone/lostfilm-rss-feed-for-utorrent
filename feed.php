a9c13747356c0173706fd0aa970b77bac1b51af9
Кладовая <r57zone@gmail.com>
2017-02-22 16:53:48 +0400

    Update

--- feed.php
+++ feed.php
@@ -1,8 +1,10 @@
 <?php 
 	$url=htmlspecialchars($_GET['url']);
 	if ($url=='') $url='http://retre.org/rssdd.xml';
+
+
 	//Заголовки для загрузки торрентов
-	$headers_a=array('http'=>array('method'=>"GET",'header'=>"Cookie: uid=xxx;pass=xxx;usess=xxxc\r\n"));
+	$headers_a=array('http'=>array('method'=>"GET",'header'=>"Cookie: uid=0;pass=0;usess=0\r\n"));
 	$headers=stream_context_create($headers_a);
 	if ($url!='http://www.lostfilm.tv/rssdd.xml') $url=str_replace('!',"&amp;",$url);
 	$source=file_get_contents($url, false, $headers);
